eb2ed0afbb9a7c93991c70053190138af064c693
admin <admin@163.com>
2019-10-26 23:35:50 +0800

    activemq

--- src/main/java/com/monkey/MonkeyApplication.java
+++ src/main/java/com/monkey/MonkeyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,8 +17,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.monkey.mapper")
-@EnableScheduling   //开启定时任务
-@EnableAsync        //开启异步任务
+//@EnableScheduling   //开启定时任务
+//@EnableAsync        //开启异步任务
+@EnableJms
 public class MonkeyApplication extends SpringBootServletInitializer{
 	
     public static void main( String[] args ){
--- src/main/java/com/monkey/config/MonkeyConfig.java
+++ src/main/java/com/monkey/config/MonkeyConfig.java
@@ -2,15 +2,22 @@ package com.monkey.config;
 
 import java.util.Arrays;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
+import javax.jms.Topic;
 import javax.servlet.MultipartConfigElement;
 import javax.sql.DataSource;
 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.util.unit.DataSize;
 
 import com.alibaba.druid.pool.DruidDataSource;
@@ -46,5 +53,23 @@ public class MonkeyConfig {
 	public DataSource druidDataSource() {
 		DruidDataSource dataSource = new DruidDataSource();
 		return dataSource;
+	}
+	
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("monkey.queue");
+	}
+	
+	@Bean
+	public Topic topic() {
+		return new ActiveMQTopic("monkey.topic");
+	}
+	
+	@Bean
+	public JmsListenerContainerFactory<?> jmsListenerContainerTopic(ConnectionFactory activeMQConnectionFactory) {
+		DefaultJmsListenerContainerFactory bean = new DefaultJmsListenerContainerFactory();
+		bean.setPubSubDomain(true);
+		bean.setConnectionFactory(activeMQConnectionFactory);
+		return bean;
 	}
 }
